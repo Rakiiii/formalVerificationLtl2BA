8222bc8a49fb7d6d0760afb7dcb888f0ff055e8d
Evgeniy Sudarskiy <ESSudarskiy@sberbank.ru>
2021-12-01 18:42:16 +0300

    Initial Commit

---
+++ LtlToBuhiAutomato/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LtlToBuhiAutomato
+//
+//  Created by Евгений Сударский on 01.12.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
